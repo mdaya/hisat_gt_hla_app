530d4add1fe2d5242e3a7440375970cced47c5a8
dayam <dayam@Xs-MacBook-Air.local>
2020-04-27 15:23:19 -0600

    Added binding for crai file

--- type_hla.sh
+++ type_hla.sh
@@ -3,6 +3,7 @@
 #Set the input file names
 ref_file_tar_name=$1
 cram_file_name=$2
+crai_file_name=$3
 
 #Extract sample ID from input CRAM
 sample_id=`basename $cram_file_name | cut -f1 -d"."`
@@ -15,7 +16,7 @@ rm -r ${base_name}
 
 #Extract reads from chr6 MHC region and all unmapped reads to fastq
 ls -alh $cram_file_name
-ls -alh ${cram_file_name}.crai
+ls -alh $crai_file_name
 touch ${sample_id}_MHC.bam 
 touch ${sample_id}_unmapped.bam 
 touch ${sample_id}_extract.bam 
