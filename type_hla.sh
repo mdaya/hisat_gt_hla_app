bfd9d87959b20774932b0fd6b4017a6710811495
dayam <dayam@Xs-MacBook-Air.local>
2020-04-27 16:33:44 -0600

    Changes to test if crai file is copied correctly

--- type_hla.sh
+++ type_hla.sh
@@ -3,7 +3,6 @@
 #Set the input file names
 ref_file_tar_name=$1
 cram_file_name=$2
-crai_file_name=$3
 
 #Extract sample ID from input CRAM
 sample_id=`basename $cram_file_name | cut -f1 -d"."`
@@ -16,7 +15,7 @@ rm -r ${base_name}
 
 #Extract reads from chr6 MHC region and all unmapped reads to fastq
 ls -alh $cram_file_name
-ls -alh $crai_file_name
+ls -alh ${cram_file_name}.crai
 touch ${sample_id}_MHC.bam 
 touch ${sample_id}_unmapped.bam 
 touch ${sample_id}_extract.bam 
