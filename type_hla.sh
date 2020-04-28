978f90a9903ae0ac6d1d3b50deff47c8ddfb20c3
dayam <dayam@Xs-MacBook-Air.local>
2020-04-27 19:49:12 -0600

    Committing real code for typing HLA

--- type_hla.sh
+++ type_hla.sh
@@ -14,17 +14,20 @@ mv ${base_name}/* .
 rm -r ${base_name}
 
 #Extract reads from chr6 MHC region and all unmapped reads to fastq
-ls -alh $cram_file_name
-ls -alh ${cram_file_name}.crai
-touch ${sample_id}_MHC.bam 
-touch ${sample_id}_unmapped.bam 
-touch ${sample_id}_extract.bam 
-touch ${sample_id}_extract.bam.1.fq.gz 
-touch ${sample_id}_extract.bam.2.fq.gz 
+samtools view -F 4 -bo ${sample_id}_MHC.bam $cram_file_name chr6:2700000-35000000
+samtools view -f 4 -bo ${sample_id}_unmapped.bam $cram_file_name
+samtools merge ${sample_id}_extract.bam ${sample_id}_MHC.bam ${sample_id}_unmapped.bam
+java -Xmx4G -jar /home/biodocker/picard/picard.jar SamToFastq \
+   I=${sample_id}_extract.bam \
+   FASTQ=${sample_id}_extract.bam.1.fq.gz SECOND_END_FASTQ=${sample_id}_extract.bam.2.fq.gz \
+   NON_PF=true RE_REVERSE=true VALIDATION_STRINGENCY=LENIENT
 
 #Extract reads that map to HLA
-touch ${sample_id}_extract.bam.1.fq.gz-hla-extracted-1.fq.gz
-touch ${sample_id}_extract.bam.1.fq.gz-hla-extracted-2.fq.gz
+hisatgenotype_toolkit extract-reads --base hla \
+   -1 ${sample_id}_extract.bam.1.fq.gz -2 ${sample_id}_extract.bam.2.fq.gz
 
 #Type HLA
-touch assembly_graph-hla-${sample_id}_extract.report
+hisatgenotype_toolkit locus -x genotype_genome \
+   --base hla \
+   --locus-list A,B,C,DRB1,DQA1,DQB1 \
+   -1 ${sample_id}_extract.bam.1.fq.gz-hla-extracted-1.fq.gz -2 ${sample_id}_extract.bam.1.fq.gz-hla-extracted-2.fq.gz
